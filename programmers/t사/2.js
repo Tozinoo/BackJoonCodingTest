9458c22a5e7902e5367573a76c0bc4c55cb36cb2
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2022-06-18 01:10:27 +0900

    2번 문제

--- programmers/t사/2.js
+++ programmers/t사/2.js
@@ -2,58 +2,32 @@ function solution(line1, line2) {
     let answer = 0;
     let n = Math.floor((line1.length - 3) / 2);
     let regExp;
-    n;
+
     for (let i = 0; i <= n; i++) {
-        if (i === 0) {
-            regExp = new RegExp(`${line2}`, "gi");
-        } else {
-            let str = "";
-            for (const key in line2) {
-                str += line2[key] + ".".repeat(i);
+        let str = "";
+        let line1Index = 0;
+        let line1String = line1;
+
+        for (const key in line2) {
+            str += line2[key] + ".".repeat(i);
+        }
+        str = str.substring(0, 2 * i + 3);
+
+        regExp = new RegExp(`${str}`);
+
+        do {
+            let findString = line1String.match(regExp);
+            if (findString) {
+                line1String = line1String.substr(findString.index + 1);
+                answer += 1;
             }
-            str;
-            str = str.substring(0, 2 * i + 3);
-
-            regExp = new RegExp(`${str}`, "gi");
-        }
-        if (line1.match(regExp) !== null) {
-            answer += line1.match(regExp).length;
-        }
-        console.log();
-        answer;
-
-        let test2 = line1.match(new RegExp(regExp, "gi"));
-        test2;
-        console.log(line2[i] * line2.length);
-        // let regExp = new RegExp(`${line2}`, "g");
+            line1Index++;
+        } while (line1Index < line1.length);
     }
-    let b = [...line1.matchAll(/b.b.b/gi)];
-    b;
-    console.log([...b]);
-    console.log(b);
-
-    let regExp2 = new RegExp(`b.b.b`, "g");
-
-    let test3 = line1.match(regExp2);
-
-    test3;
-
-    console.log(line1.match(line2));
-
-    console.log(line1.length - line2.length + 1);
-
-    for (const v in line1) {
-        v;
-        console.log(line1[v]);
-        if (line1[v] === line2) answer++;
-    }
-    answer;
 
     return answer;
 }
 
-// 3n + 2;
-
-console.log(solution("abbbcbbb", "bbb"));
-//console.log(solution("abcabcabc", "abc"));
-// console.log("abacaba", "acb");
+// console.log(solution("abbbcbbb", "bbb"));
+// console.log(solution("abcabcabc", "abc"));
+console.log(solution("abacaba", "acb"));
